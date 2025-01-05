272bf103d39c18d07a61e1fe1eed92d57dd8ad82
alisalali <alis.alali@hotmail.com>
2025-01-05 18:38:12 +0300

    05-changing-data-with-mutations

--- src/util/http.js
+++ src/util/http.js
@@ -1,12 +1,10 @@
 export async function fetchEvents({ signal, searchTerm }) {
   let url = "http://localhost:3000/events";
-  console.log(searchTerm);
 
   if (searchTerm) {
     url += "?search=" + searchTerm;
   }
   const response = await fetch(url, { signal });
-  console.log(url);
   if (!response.ok) {
     const error = new Error("An error occurred while fetching the events");
     error.code = response.status;
@@ -17,4 +15,25 @@ export async function fetchEvents({ signal, searchTerm }) {
   const { events } = await response.json();
 
   return events;
+}
+
+export async function createNewEvent(eventData) {
+  const response = await fetch(`http://localhost:3000/events`, {
+    method: "POST",
+    body: JSON.stringify(eventData),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  if (!response.ok) {
+    const error = new Error("An error occurred while creating the event");
+    error.code = response.status;
+    error.info = await response.json();
+    throw error;
+  }
+
+  const { event } = await response.json();
+
+  return event;
 }

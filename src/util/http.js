b2802e77e6f689cb01cbf8c35157c6023aa568c7
alisalali <alis.alali@hotmail.com>
2025-01-05 18:04:20 +0300

    04-enabling-disabling-queries

--- src/util/http.js
+++ src/util/http.js
@@ -1,6 +1,12 @@
-export async function fetchEvents() {
-  const response = await fetch("http://localhost:3000/events");
+export async function fetchEvents({ signal, searchTerm }) {
+  let url = "http://localhost:3000/events";
+  console.log(searchTerm);
 
+  if (searchTerm) {
+    url += "?search=" + searchTerm;
+  }
+  const response = await fetch(url, { signal });
+  console.log(url);
   if (!response.ok) {
     const error = new Error("An error occurred while fetching the events");
     error.code = response.status;
